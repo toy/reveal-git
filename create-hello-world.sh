cf3ea8ea96e7578fc44660f46b1e2f6a3ca9a991
Ivan Kuchin <ivan.kuchin@cern.ch>
2013-05-07 01:54:21 +0200

    fragmented hands-on

--- create-hello-world.sh
+++ create-hello-world.sh
@@ -24,6 +24,14 @@ function hsection(){
   echo '  %h3 '$*
 }
 
+function fragment(){
+  echo '  .fragment'
+  while read line
+  do
+    echo "    $line"
+  done
+}
+
 hsection 'Identify yourself'
 echo '  :markdown'
 echo "      $ git config --global user.name 'Ivan Kuchin'"
@@ -55,57 +63,94 @@ git config color.status always
 git config color.ui always
 
 hsection 'Commit first file'
-echo '  %p Create file README with content:'
-hfile 'Prints "Hello, World".' > README
-echo '  %p Stage'
-hcat git add README
-echo '  %p Commit'
-hcat "git commit --message 'README'"
+(
+  echo '  %p Create file README with content:'
+  hfile 'Prints "Hello, World".' > README
+) 2>&1 | fragment
+(
+  echo '  %p Stage'
+  hcat git add README
+) | fragment
+(
+  echo '  %p Commit'
+  hcat "git commit --message 'README'"
+) | fragment
 
 hsection 'Checkout new branch'
 hcat git checkout -b implementation
+
 hsection 'Start implementing'
-echo '  %p Create file HelloWorld.java with content:'
-hfile 'public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello, World")
-  }
-}' > HelloWorld.java
-echo '  %p Stage'
-hcat git add HelloWorld.java
-echo '  %p Commit'
-hcat "git commit --message 'Implemented HelloWorld'"
+(
+  echo '  %p Create file HelloWorld.java with content:'
+  hfile 'public class HelloWorld {
+    public static void main(String[] args) {
+      System.out.println("Hello, World")
+    }
+  }' > HelloWorld.java
+) 2>&1 | fragment
+(
+  echo '  %p Stage'
+  hcat git add HelloWorld.java
+) | fragment
+(
+  echo '  %p Commit'
+  hcat "git commit --message 'Implemented HelloWorld'"
+) | fragment
 
 hsection 'Test and fix'
-echo '  %p Ops'
-hcat javac HelloWorld.java
-echo '  %p Fix HelloWorld.java:'
-hfile 'System.out.println("Hello, World!");' > /dev/null
-echo 'public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello, World!");
-  }
-}' > HelloWorld.java
-echo '  %p It works!'
-hcat "javac HelloWorld.java && java HelloWorld"
+(
+  echo '  %p Oops…'
+  hcat javac HelloWorld.java
+) | fragment
+(
+  echo '  %p Fix HelloWorld.java:'
+  hfile 'System.out.println("Hello, World!");' > /dev/null
+  echo 'public class HelloWorld {
+    public static void main(String[] args) {
+      System.out.println("Hello, World!");
+    }
+  }' > HelloWorld.java
+) 2>&1 | fragment
+(
+  echo '  %p It works!'
+  hcat "javac HelloWorld.java && java HelloWorld"
+) | fragment
 
 hsection 'Ignore build results'
-hcat git status
-echo '  %p Create file .gitignore with content:'
-hfile '/*.class' > .gitignore
+(
+  echo '  %p Check status'
+  hcat git status
+) | fragment
+(
+  echo '  %p Create file .gitignore with content:'
+  hfile '/*.class' > .gitignore
+) 2>&1 | fragment
 
 hsection 'Commit changes'
-echo '  %p Amend last commit with fix'
-hcat git add HelloWorld.java
-hcat "git commit --amend --message 'Implemented HelloWorld'"
-echo '  %p Commit ignore pattern'
-hcat git add .gitignore
-hcat "git commit --message 'ignore /*.class'"
+(
+  echo '  %p Amend last commit with fix'
+  hcat git add HelloWorld.java
+  hcat "git commit --amend --message 'Implemented HelloWorld'"
+) | fragment
+(
+  echo '  %p Commit ignore pattern'
+  hcat git add .gitignore
+  hcat "git commit --message 'ignore /*.class'"
+) | fragment
 
 hsection 'Merge and delete branch'
-hcat git co master
-hcat "git merge --no-ff implementation --message 'merge initial implementation'"
-hcat git branch -d implementation
+(
+  echo '  %p Switch to master'
+  hcat git co master
+) | fragment
+(
+  echo '  %p Merge branch'
+  hcat "git merge --no-ff implementation --message 'merge initial implementation'"
+) | fragment
+(
+  echo '  %p Delete branch'
+  hcat git branch -d implementation
+) | fragment
 
 hsection 'Show history'
 hcat "git log --graph --all --format=format:'%C(bold yellow)%h%C(reset) - %C(white)%s%C(reset) %C(bold white)— %an%C(reset)%C(bold yellow)%d%C(reset)%n'"
